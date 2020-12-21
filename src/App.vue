a804e834cd00c482d4fcb6dc2cf473da3dd02b3a
yukinessa <kun.amrin@gmail.com>
2020-12-21 11:22:35 +0700

    fix(): fix css on every single section

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,15 @@
 <template>
+<div id="app">
   <v-app>
-    <v-app-bar app color="Dark" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Logo-navbar"
-          class="shrink mr-2"
-          contain
-          src="./assets/logo-white.png"
-          transition="scale-transition"
-          width="80"
-        />
-      </div>
-      <v-spacer></v-spacer>
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Home</span>
-      </v-btn>
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">News</span>
-      </v-btn>
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Iklan</span>
-      </v-btn>
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Home</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <div class="black">
-      <v-container>
-        <Highlight  />
-        <br>
-        <Category />
-        <br>
-        <br>
-        <Advertise />
-        <br>
-        <infoBarang />
-        <br>
-        <infoSosial />
-        <br>
-        <Advertise />
-        <br>
-        <Youtube />
-        <br>
-        <Advertise2 />
-        <br>
-        <Footer />
-      </v-container>
-      <router-view></router-view>
-      </div>
-    </v-main>
+    <router-view></router-view>
   </v-app>
+</div>
 </template>
 
 <script>
-import Highlight from "./components/Highlight";
-import Category from "./components/Category";
-import Advertise from "./components/Advertise";
-import infoBarang from "./components/infoBarang";
-import infoSosial from "./components/infoSosial";
-import Youtube from "./components/Youtube";
-import Advertise2 from "./components/Advertise2";
-import Footer from "./components/Footer";
+
 
 export default {
-  name: "App",
-  components: {
-    Highlight,
-    Category,
-    Advertise,
-    infoBarang,
-    infoSosial,
-    Youtube,
-    Advertise2,
-    Footer
-  },
-  data: () => ({
-    //
-  })
-};
+  
+}
 </script>
