f3cff5784aea78585873c2c5ad6c03fdbaba954b
abdurahmanrizal <abdurahmanrizal1@gmail.com>
2021-01-20 16:42:10 +0700

    feat(): component category for mobile

---
+++ src/components/CategoryMobile.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-container>
+    <h5 style="color:white;" class="font-weight-bold text-center mb-4">
+      Channel Kami
+    </h5>
+    <splide :slides="category" :options="options" style="margin-left:-.5rem">
+      <splide-slide v-for="item in category" :key="item.id">
+        <router-link :to="'/detail-category/' + item.id">
+          <v-img
+            :class="`rounded-lg`"
+            :src="urlImg + 'channel_category/' + item.url"
+            style="width: 100%"
+          ></v-img>
+        </router-link>
+      </splide-slide>
+    </splide>
+  </b-container>
+</template>
+<script>
+import loadImg from "../../config";
+export default {
+  data() {
+    return {
+      category: [],
+      urlImg: loadImg,
+      options: {
+        perPage: 1,
+        pagination: false,
+        gap: ".5rem",
+        fixedWidth: "15rem",
+        arrows: false,
+      },
+    };
+  },
+  mounted() {
+    this.getCategory();
+  },
+  methods: {
+    getCategory() {
+      this.axios
+        .get(process.env.VUE_APP_IP_ADDRESS + "youtube/channel/")
+        .then((response) => {
+          this.category = response.data.datas;
+        });
+    },
+  },
+};
+</script>
