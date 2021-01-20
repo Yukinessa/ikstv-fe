8406f2b23892bb8f5e69959148c75a9607369f21
abdurahmanrizal <abdurahmanrizal1@gmail.com>
2021-01-20 16:47:40 +0700

    feat(): router for mobile display

--- src/router/index.js
+++ src/router/index.js
@@ -19,14 +19,33 @@ const routes = [
     component: () => import(/* webpackChunkName: "News" */ "../views/News.vue"),
   },
   {
+    path: "/news/mobile/:title",
+    name: "NewsMobile",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "News" */ "../views/NewsMobile.vue"),
+  },
+  {
     path: "/list-lost",
     name: "listLost",
     component: () => import("../views/listLost.vue"),
   },
   {
+    path: "/list-lost/mobile",
+    name: "listLostMobile",
+    component: () => import("../views/listLostMobile.vue"),
+  },
+  {
     path: "/list-number",
     name: "listNumber",
     component: () => import("../views/listNumber.vue"),
+  },
+  {
+    path: "/list-number/mobile",
+    name: "listNumberMobile",
+    component: () => import("../views/listNumberMobile.vue"),
   },
   {
     path: "/detail-category/:playlist",
@@ -42,6 +61,16 @@ const routes = [
     path: "/list-loker",
     name: "listLoker",
     component: () => import("../views/listLoker.vue"),
+  },
+  {
+    path: "/list-loker/mobile",
+    name: "listLokerMobile",
+    component: () => import("../views/listLokerMobile.vue"),
+  },
+  {
+    path: "/list-sosial/mobile",
+    name: "listSosialMobile",
+    component: () => import("../views/listSosialMobile.vue"),
   },
 ];
 
