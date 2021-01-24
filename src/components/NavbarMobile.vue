eac56bcda8663f154d128decfee904c2f715f714
abdurahmanrizal <abdurahmanrizal1@gmail.com>
2021-01-24 15:54:57 +0700

    feat(): component navbar mobile

---
+++ src/components/NavbarMobile.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-navbar toggleable="lg" type="dark">
+    <b-navbar-brand>
+      <router-link :to="'/'">
+        <v-img
+          alt="Logo-navbar"
+          class="shrink mr-2"
+          contain
+          src="../assets/logo-white.png"
+          width="60"
+        />
+      </router-link>
+    </b-navbar-brand>
+  </b-navbar>
+</template>
