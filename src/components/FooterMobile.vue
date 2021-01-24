cdd3a263a3ccf0e19e12384e6005e1dab67fe81a
abdurahmanrizal <abdurahmanrizal1@gmail.com>
2021-01-24 15:54:17 +0700

    refactor(): element footer to bottom navigation

--- src/components/FooterMobile.vue
+++ src/components/FooterMobile.vue
@@ -1,82 +1,36 @@
 <template>
-  <div class="footer" style="background-color: red">
-    <b-container>
-      <b-row>
-        <b-col sm="1" class="p-2">
-          <img
-            src="@/assets/logo-white.png"
-            width="100"
-            style="display: block; margin-left:auto; margin-right: auto"
-          />
-          <!-- </a> -->
-        </b-col>
-        <b-col sm="1">
-          <p class="text-light text-justify" style="line-height: 2rem">
-            Kami memberikan informasi se-kota semarang mulai dari info
-            kecelakaan hingga info sosial. Ikuti pembaruan berita kami melalui
-            website dan sosial media yang tersedia
-          </p>
-        </b-col>
-        <b-col sm="1" class="text-center" style="line-height: 4rem">
-          <h5 class="font-weight-bold text-light">
-            EXPLORE
-          </h5>
-          <a id="beranda" class="text-light" style="font-size:15px;" href=""
-            >Beranda</a
-          >
+  <v-bottom-navigation fixed background-color="grey darken-4">
+    <v-btn value="home" :to="'/'">
+      <span style="color: white">Beranda</span>
 
-          <a
-            id="news"
-            class="text-light"
-            style="font-size:15px; display:block"
-            @click="test('news')"
-            >Berita Terkini</a
-          >
-          <a
-            id="lost-item"
-            class="text-light"
-            style="font-size:15px; display:block"
-            href=""
-            >Info Kehilangan</a
-          >
-          <a
-            id="iklan"
-            class="text-light"
-            style="font-size:15px; display:block"
-            href=""
-            >Iklan</a
-          >
-        </b-col>
-        <b-col sm="1">
-          <h5 style="color:white;  text-align:center">
-            FOLLOW US
-          </h5>
-          <div style="text-align:center; margin-top: 1rem">
-            <a href="">
-              <img src="@/assets/icon-instagram.png" width="30" />
-            </a>
-            <a href="">
-              <img
-                src="@/assets/icon-youtube.png"
-                width="30"
-                style="margin-left:1rem"
-              />
-            </a>
-            <a href="">
-              <img
-                src="@/assets/icon-wa.png"
-                width="25"
-                style="margin-left:1rem"
-              />
-            </a>
-          </div>
-        </b-col>
-        <b-col sm="1" class="mt-5 text-light text-center font-weight-bold">
-          &copy; {{ year }} Info Kejadian Semarang</b-col
-        >
-      </b-row>
-    </b-container>
-  </div>
+      <v-icon small style="color: white">mdi-home</v-icon>
+    </v-btn>
+    <v-btn>
+      <span style="color: white">Berita</span>
+
+      <v-icon small style="color: white">mdi-newspaper-variant-outline</v-icon>
+    </v-btn>
+
+    <v-btn :to="'/list-loker/mobile'">
+      <span style="color: white">Loker</span>
+
+      <v-icon small style="color: white"
+        >mdi-clipboard-text-search-outline</v-icon
+      >
+    </v-btn>
+
+    <v-btn :to="'/list-sosial/mobile'">
+      <span style="color: white">Sosial</span>
+
+      <v-icon small style="color: white">mdi-account-group</v-icon>
+    </v-btn>
+    <!-- <router-link :to="'/list-lost/mobile'" tag="button"> -->
+    <v-btn :to="'/list-lost/mobile'">
+      <span style="color: white">Kehilangan</span>
+      <v-icon small style="color: white">mdi-chat-question-outline</v-icon>
+    </v-btn>
+    <!-- </router-link> -->
+  </v-bottom-navigation>
 </template>
 <script>
 import moment from "moment";
@@ -88,3 +42,8 @@ export default {
   },
 };
 </script>
+<style>
+.v-item-group.v-bottom-navigation .v-btn {
+  min-width: 30px !important;
+}
+</style>
