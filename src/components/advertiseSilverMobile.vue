7bb34957cba518fdec771c06096cc1c4c27d6afc
abdurahmanrizal <abdurahmanrizal1@gmail.com>
2021-02-03 16:25:49 +0700

    feat(): advertise silver mobile

---
+++ src/components/advertiseSilverMobile.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <splide :slides="items" :options="options">
+      <splide-slide v-for="(item, i) in items" :key="i">
+        <b-img
+          v-if="item.photo != null"
+          :src="urlImg + '/iklan/' + item.photo"
+          :class="`rounded-lg`"
+          class="shadow"
+          style="width:280px; height: 159px;"
+        >
+        </b-img>
+      </splide-slide>
+    </splide>
+  </div>
+</template>
+
+<script>
+import loadImg from "../../config";
+export default {
+  data() {
+    return {
+      items: [],
+      urlImg: loadImg,
+      options: {
+        type: "loop",
+        perPage: 1,
+        perMove: 1,
+        pagination: false,
+        gap: ".5rem",
+        autoWidth: true,
+        focus: "center",
+        autoplay: true,
+        padding: {
+          right: "5rem",
+          left: "5rem",
+        },
+        // start: 2,
+        rewind: true,
+        trimSpace: false,
+      },
+    };
+  },
+  mounted() {
+    this.getAdvertise();
+  },
+  methods: {
+    getAdvertise() {
+      this.axios
+        .get(process.env.VUE_APP_IP_ADDRESS + "iklan/")
+        .then((response) => {
+          this.items = response.data.silver;
+        });
+    },
+  },
+};
+</script>
+
+<style>
+.v-carousel {
+  height: 300px !important ;
+}
+</style>
