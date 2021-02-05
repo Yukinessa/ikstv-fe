10889ecf8517656ccf2355b9b2c303c7612d6cca
abdurahmanrizal <abdurahmanrizal1@gmail.com>
2021-02-05 16:28:51 +0700

    feat(): condition if length content 0

--- src/views/detailCategory.vue
+++ src/views/detailCategory.vue
@@ -6,40 +6,58 @@
         {{ categoryChannel.name }}
       </h1>
       <div id="nav" style="text-align: center; color white"></div>
-      <div
-        style="margin-top:1rem; padding-bottom:1rem"
-        v-for="(item, i) in content"
-        :key="i"
-      >
-        <v-card>
-          <v-row>
-            <v-col cols="4" md="3">
-              <v-img
-                style="margin-left:3rem; margin-top:1rem; margin-bottom:1rem"
-                :src="urlImg + 'channel/' + item.thumbnail"
-                :class="`rounded-lg`"
-                max-width="180"
-              ></v-img>
-            </v-col>
-            <v-col cols="8" md="6">
-              <h2 style="margin-top:1rem; color:white">
-                {{ item.title }}
-              </h2>
-              <p style="font-size:12px; color:grey">
-                {{ item.created_at | moment("dddd, MMMM Do YYYY") }}
-              </p>
-              <p style="color:white; text-align:justify">
-                {{ limitText(item.description) }}
-              </p>
-              <v-btn small color="error" @click="goYoutube(item.youtube_url)">
-                <v-icon dark>
-                  mdi-youtube
-                </v-icon>
-                <span class="ml-1">Youtube</span>
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-card>
+      <div v-if="content.length == 0" class="mt-5">
+        <v-img
+          style="margin-left:3rem; margin-top:1rem; margin-bottom:1rem"
+          :src="require('../assets/icon.png')"
+          :class="`rounded-lg`"
+          height="auto"
+          class="d-block mx-auto"
+          width="500px"
+        ></v-img>
+        <h4 class="text-light text-center">Urung Ana Konten Lur...</h4>
+      </div>
+      <div v-else>
+        <div
+          style="margin-top:1rem; padding-bottom:1rem"
+          v-for="(item, i) in content"
+          :key="i"
+        >
+          <v-card>
+            <v-row>
+              <v-col cols="3">
+                <v-img
+                  style="margin-left:3rem; margin-top:1rem; margin-bottom:1rem"
+                  :src="urlImg + '/channel/' + item.thumbnail"
+                  :class="`rounded-lg`"
+                  height="auto"
+                  width="100%"
+                ></v-img>
+              </v-col>
+              <v-col cols="9">
+                <div class="ml-10">
+                  <h4 style="margin-top:1rem; color:white">
+                    {{ item.title }}
+                  </h4>
+                  <p style="font-size:12px; color:grey">
+                    {{ convertDate(item.created_at) }}
+                  </p>
+
+                  <v-btn
+                    small
+                    color="error"
+                    @click="goYoutube(item.youtube_url)"
+                  >
+                    <v-icon dark>
+                      mdi-youtube
+                    </v-icon>
+                    <span class="ml-1">Youtube</span>
+                  </v-btn>
+                </div>
+              </v-col>
+            </v-row>
+          </v-card>
+        </div>
       </div>
       <div style="padding-top:6rem">
         <Footer />
@@ -52,6 +70,7 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import loadImg from "../../config.js";
+import moment from "moment";
 
 export default {
   data() {
@@ -64,6 +83,11 @@ export default {
   components: {
     Navbar,
     Footer,
+  },
+  mounted() {
+    this.getContent();
+    this.getChannelCategory();
+    moment.locale("id");
   },
   methods: {
     limitText(text) {
@@ -95,11 +119,9 @@ export default {
           this.categoryChannel = response.data.result;
         });
     },
-  },
-  mounted() {
-    this.getContent();
-    this.getChannelCategory();
-    console.log(this.$route.params.playlist);
+    convertDate(strDate) {
+      return moment(strDate).format("dddd, DD MMMM YYYY");
+    },
   },
 };
 </script>
