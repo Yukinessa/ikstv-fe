c8fa25d1a439f8eff8175e9108f43a0f75bd933c
abdurahmanrizal <abdurahmanrizal1@gmail.com>
2021-03-12 11:36:03 +0700

    feat(): edit var loadImg, urlLink

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const loadImg = "http://147.139.162.60:9000/uploads";
-const urlLink = "http://147.139.162.60:9000/";
+const loadImg = "http://admin.infokejadiansemarang.com/uploads/";
+const urlLink = window.location.origin;
 
 export { loadImg, urlLink };
